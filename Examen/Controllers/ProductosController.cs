d9122d90755118abe50008ac9c2cd9eda111a6fc
hazeldex <152042208+hazeldex@users.noreply.github.com>
2023-12-07 21:32:05 -0600

    AtualizarProducto

--- Examen/Controllers/ProductosController.cs
+++ Examen/Controllers/ProductosController.cs
@@ -84,10 +84,11 @@ namespace Examen.Controllers
 
                 }
             }
-            catch
+            catch (Exception e)
             {
-
-
+                ViewBag.ValorMensaje = 1;
+                ViewBag.MensajeProducto = "Fallo la actualizacion" + e ;
+                return View(produc);
             }
 
         }
